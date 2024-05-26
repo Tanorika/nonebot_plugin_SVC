c257708ff8f03a96f0c8799012eb3a38b60b9d8b
Tanorika <234735543@qq.com>
2024-05-26 13:40:32 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,6 +11,7 @@ import sys
 sys.path.append('nonebot_plugin_SVC')
 from .bot import texttosound
 import os,re
+from nonebot.params import ArgPlainText
 from nonebot.adapters.cqhttp import MessageSegment
 current_path = os.path.dirname(__file__)
 from nonebot.adapters.onebot.v11 import (
@@ -24,10 +25,16 @@ characterlist=[]
 for name in config.keys():
         characterlist.append(name)
 #默认使用的模型
-defalt='defaltmodel'
+defalt='瑶瑶'
 time=datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
 key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r'Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders')
 desktoppath = winreg.QueryValueEx(key, "Desktop")[0]
+
+number=0
+modelcard=""
+for name in config.keys():
+    number=number+1
+    modelcard+=str(number)+"."+name+"\n"
 
 async def send_to_tts(msg,character):
     if character in config:
@@ -68,23 +75,28 @@ async def _(event: MessageEvent):
         await reply.finish(MessageSegment.text('唔……说不出，检查一下是不是默认模型设置出错了'))
 
 
-change = on_command("/让",rule = to_me(), priority=99, block=False)
+change = on_command("/选择模型",rule = to_me(), priority=99, block=False)
 @change.handle()
 async def _(event: MessageEvent):
     # 获取消息文本
     msg = str(event.get_message())
     # 去掉带中括号的内容(去除cq码)
     msg = re.sub(r"\[.*?\]|\s*", "", msg)
-    parts=msg.split("说")
-    character= parts[0][2:]
+
+@change.got("character",prompt="以下是支持的声音列表:\n"+modelcard)
+async def getchar(event:MessageEvent):
+    pass
+@change.got("content",prompt="请输入要输出的内容")
+async def getcontent(character: str = ArgPlainText("character"),
+                     content: str = ArgPlainText("content")):
     if character.isdigit()==True :
         character = characterlist[int(character)-1]
-    msg= parts[1]
-    preview=msg[:10]
-    if(await send_to_tts(msg,character)!='NotFound'):
+    if character in config:
+        await send_to_tts(content,character)
         await change.finish(MessageSegment.record(Path(current_path+"\\results\\qqbot.wav")))
     else:
-        await change.finish(MessageSegment.text('唔……是不是打错名称了？'))
+        await change.finish(MessageSegment.text('唔……模型检索失败……是不是打错名称了？'))
+    
 
 
 save = on_command("/存",rule = to_me(), priority=99, block=False)
@@ -94,28 +106,29 @@ async def _(event: MessageEvent):
     msg = str(event.get_message())
     # 去掉带中括号的内容(去除cq码)
     msg = re.sub(r"\[.*?\]|\s*", "", msg)
-    parts=msg.split("说")
-    character= parts[0][2:]
+
+@save.got("character",prompt=modelcard)
+async def getchar(event:MessageEvent):
+    pass
+@save.got("content",prompt="请输入要输出的内容")
+async def getcontent(character: str = ArgPlainText("character"),
+                     content: str = ArgPlainText("content")):
+    preview=content[:10]
     if character.isdigit()==True :
         character = characterlist[int(character)-1]
-    msg= parts[1]
-    preview=msg[:10]
-    if(await send_to_tts(msg,character)!='NotFound'):
+    if character in config:
+        await send_to_tts(content,character)
         if not os.path.exists(desktoppath+"\\QQBotSave\\"):
             os.mkdir(desktoppath+"\\QQBotSave\\")
         shutil.copy(current_path+"\\results\\qqbot.wav",desktoppath+"\\QQBotSave\\")
         os.rename(desktoppath+"\\QQBotSave\\"+"qqbot.wav",desktoppath+"\\QQBotSave\\"+character+"-"+preview+"-"+time+".wav")
         await save.finish(MessageSegment.record(Path(current_path+"\\results\\qqbot.wav")))
     else:
-        await save.finish(MessageSegment.text('唔……是不是打错名称了？'))
+        await change.finish(MessageSegment.text('唔……模型检索失败……是不是打错名称了？'))
 
 
 Listname = on_command("/列表",aliases={"/声音列表"},rule = to_me(), priority=99, block=False)
 @Listname.handle()
 async def _():
-    number=0
-    msg=""
-    for name in config.keys():
-        number=number+1
-        msg+=str(number)+"."+name+"\n"
-    await Listname.finish(MessageSegment.text("以下是支持的声音列表:\n"+msg+"\n使用指令“/让……说 内容”可以用不同声音输出，用序号表示也可以\n例：让1说 你好\n直接/说 内容 默认使用瑶瑶\n使用“/存……说 可将录音保存至服务器桌面并发送录音”"))
+    
+    await Listname.finish(MessageSegment.text("以下是支持的声音列表:\n"+modelcard+"\n使用指令“/选择模型 可以用不同声音输出，用序号表示也可以，默认使用瑶瑶\n使用“/存 可将录音保存至服务器桌面并发送录音"))
